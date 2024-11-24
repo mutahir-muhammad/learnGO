30b9858fea009d4e0df9dba797613464b866faf6
Mutahir <mutahir.khan44@yahoo.com>
2024-11-24 23:11:22 +0500

    made changes to the file on day2

--- cmd/tut_01/main.go
+++ cmd/tut_01/main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	// basic vars
 	var a int = 10
 	var b int = 20
 	var c int = a + b
 
+	// basic print
 	fmt.Println("a is", a)
 	fmt.Println("b is", b)
 	fmt.Println("Sum of a and b is", c)
@@ -15,17 +17,32 @@ func main() {
 	fmt.Println("modulo of b by a is", b%a)
 	fmt.Println("Hello, World!")
 
+	// booleans are true and false... ofc...lol
+	// string
 	var myStr string = "This is me GOing!"
 	fmt.Println(myStr)
 
+	// runes give ascii value
 	var myRuneIsRuined rune = 'a'
 	fmt.Println(myRuneIsRuined)
 	myRuneIsRuined = 'b'
 	fmt.Println(myRuneIsRuined)
 
+	// functions
 	printMe()
+	fmt.Println(intDivision(10, 2))
+	var resultDivide, resultModulo = multipleReturns(10, 3)
+	fmt.Println(resultDivide, resultModulo)
 }
 
 func printMe() {
 	fmt.Println("I am a function")
+}
+
+func intDivision(numerator int, denominator int) int {
+	return numerator / denominator
+}
+
+func multipleReturns(numerator int, denominator int) (int, int) {
+	return numerator / denominator, numerator % denominator
 }
